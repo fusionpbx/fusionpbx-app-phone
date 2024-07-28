dd282d4c27f4e54e33593f3971eba646be2ddb6b
FusionPBX <markjcrane@gmail.com>
2024-07-27 18:04:52 -0600

    Add the phone dashboard

---
+++ resources/dashboard/config.php
@@ -0,0 +1,41 @@
+<?php
+
+$array['dashboard'][$x]['dashboard_uuid'] = '2f0ef341-724e-4578-84bb-803e0fc0b14c';
+$array['dashboard'][$x]['dashboard_name'] = 'Phone';
+$array['dashboard'][$x]['dashboard_path'] = 'core/dashboard/resources/dashboard/icon.php';
+$array['dashboard'][$x]['dashboard_icon'] = 'fa-phone';
+$array['dashboard'][$x]['dashboard_url'] = '/app/phone/phone.php';
+$array['dashboard'][$x]['dashboard_target'] = 'new';
+$array['dashboard'][$x]['dashboard_width'] = '320';
+$array['dashboard'][$x]['dashboard_height'] = '480';
+$array['dashboard'][$x]['dashboard_content'] = '';
+$array['dashboard'][$x]['dashboard_content_text_align'] = '';
+$array['dashboard'][$x]['dashboard_content_details'] = '';
+$array['dashboard'][$x]['dashboard_chart_type'] = '';
+$array['dashboard'][$x]['dashboard_heading_text_color'] = '';
+$array['dashboard'][$x]['dashboard_heading_text_color_hover'] = '';
+$array['dashboard'][$x]['dashboard_heading_background_color'] = '';
+$array['dashboard'][$x]['dashboard_heading_background_color_hover'] = '';
+$array['dashboard'][$x]['dashboard_number_background_color'] = '';
+$array['dashboard'][$x]['dashboard_number_text_color'] = '#00b043';
+$array['dashboard'][$x]['dashboard_number_text_color_hover'] = '';
+$array['dashboard'][$x]['dashboard_background_color'] ='["#ffffff"]';
+$array['dashboard'][$x]['dashboard_background_color_hover'] = '';
+$array['dashboard'][$x]['dashboard_detail_background_color'] = '';
+$array['dashboard'][$x]['dashboard_column_span'] = '1';
+$array['dashboard'][$x]['dashboard_row_span'] = '1';
+$array['dashboard'][$x]['dashboard_details_state'] = 'disabled';
+$array['dashboard'][$x]['dashboard_order'] = '20';
+$array['dashboard'][$x]['dashboard_enabled'] = 'true';
+$array['dashboard'][$x]['dashboard_description'] = 'Browser based phone.';
+$y = 0;
+$array['dashboard'][$x]['dashboard_groups'][$y]['dashboard_group_uuid'] = 'a370d46f-6713-480a-90b9-2cf855817d89';
+$array['dashboard'][$x]['dashboard_groups'][$y]['dashboard_uuid'] = '2f0ef341-724e-4578-84bb-803e0fc0b14c';
+$array['dashboard'][$x]['dashboard_groups'][$y]['group_name'] = 'superadmin';
+//$y++;
+//$array['dashboard'][$x]['dashboard_groups'][$y]['dashboard_group_uuid'] = 'be25539c-468c-4e95-abd2-6a20cb9c2fd4';
+//$array['dashboard'][$x]['dashboard_groups'][$y]['dashboard_uuid'] = '2f0ef341-724e-4578-84bb-803e0fc0b14c';
+//$array['dashboard'][$x]['dashboard_groups'][$y]['group_name'] = 'admin';
+//$y++;
+
+?>
